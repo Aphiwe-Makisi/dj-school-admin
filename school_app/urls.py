14a35f7cad5b37ff0d418cfa98707e458a868eec
Aphiwe <makisiaphiwe@gmail.com>
2023-09-01 16:11:56 +0200

    Added basic routing between home and form page

--- school_app/urls.py
+++ school_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("students/", views.home, name="home"),
+    path("student_form/", views.add_new_student, name="student_form"),
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -5,3 +5,6 @@ from school_app.forms import NameForm
 # Create your views here.
 def home(request):
     return render(request, "school_app/home.html")
+
+def add_new_student(request):
+    return render(request, "school_app/student_form.html")
